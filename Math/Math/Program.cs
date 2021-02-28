e920663a8b5c17c10b3bdcedee3fbb6a22959c12
Priyanka Thakan <priyankawu@yahoo.com>
2021-02-27 20:39:16 -0800

    Math int division converted to float

---
+++ Math/Math/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 3;
+            int b = 7;
+            int sum = a + b;
+            int diff = a - b;
+            int multiply = a * b;
+            float div = ((float)a) / ((float)b);
+            Console.WriteLine("Addition:" + sum + "\n Subtraction: " + diff + "\n Multiply:" + multiply + "\n Divide: " + div);
+            Console.Read();
+        }
+    }
+}
