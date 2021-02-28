5f56c662aee0d7f79f043e5cbc482b8086dbd7c2
Priyanka Thakan <priyankawu@yahoo.com>
2021-02-27 18:22:25 -0800

    HelloWorld Assignment

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+    {
+        static void Main()     /* modifiers are objectives for methods */
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we're \" accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+
