f5b2170886796ab519f3f539d6e106f7ea19f08e
Priyanka Thakan <priyankawu@yahoo.com>
2021-03-01 10:41:18 -0800

    DowhileLoop190
    while, do while assignment on step 190

---
+++ DoWhileLoops/DoWhileLoops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DoWhileLoops
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool play_time = false;
+            int count = 0;
+            while (play_time == false)
+            {
+                Console.WriteLine("He is studying for his remote class");
+                count = count + 1;
+                Console.WriteLine(count);
+                if (count == 5)
+                {
+                    play_time = true;
+                }
+            }
+            
+            // Do While Loop: does the loop once, then checks the condition
+            play_time = false;
+            do {
+                Console.WriteLine("This is a do while loop that will print this once for sure. He is studying for his remote class");
+                play_time = true;
+            }
+            while (!play_time);
+            Console.Read();
+        }
+    }
+}
---
+++ loops/loops/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace loops
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            do
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("you guessed 62. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 32:
+                        Console.WriteLine("You guessed it right!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        isGuessed = true;
+                        break;
+                }
+
+            }
+            while (!isGuessed);
+
+            Console.Read();
+        }
+
+    }
+}
