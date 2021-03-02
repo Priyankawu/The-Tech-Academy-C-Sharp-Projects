cfc44b9903c0eb531eb19c04ddf0434c7cbc9124
Priyanka Thakan <priyankawu@yahoo.com>
2021-03-02 10:10:39 -0800

    Console App assignment part one 204

--- ConsoleApp204/ConsoleApp204/Program.cs
+++ ConsoleApp204/ConsoleApp204/Program.cs
@@ -4,8 +4,15 @@ using System.Collections.Generic;
 
     class Program
     {
-        static void Main(string[] args)
-        {
-        }
-    }
+    //I didn't understand the assignment. Does string mean a sentence or a word? Is the user writing a paragraph and my array items are each sentence?
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter a sentence.");
+        string sentence = Console.ReadLine();
+        string[] stArray = sentence.Split(' ');
 
+        foreach (string word in stArray)
+            Console.WriteLine(word);
+        Console.Read();
+    }
+    }
