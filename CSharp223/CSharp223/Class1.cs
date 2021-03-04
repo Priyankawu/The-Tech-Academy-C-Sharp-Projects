1168ea2ba960b9901f68fefac4e72ada9744a46f
Priyanka Thakan <priyankawu@yahoo.com>
2021-03-04 10:52:54 -0800

    Assignment223
    Methods practice.

---
+++ CSharp223/CSharp223/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp223
+{
+    class Class1
+    {
+
+        //Notice you did not need to put a construcor method here and the program still works. Maybe because this object has no properties.
+
+
+        public int method_multiply5(int n)
+        {
+            return n * 5;
+        }
+
+        public int method_add5(int n)
+        {
+            return n+5;
+        }
+
+        public int method_mod5(int n)
+        {
+            return n % 5;
+        }
+
+    }
+}
---
+++ CSharp223/CSharp223/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp223
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the number you want to do the math operations on?");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Class1 obj = new Class1();
+            int ans_1 = obj.method_multiply5(number);
+            int ans_2 = obj.method_add5(number);
+            int ans_3 = obj.method_mod5(number);
+
+            Console.WriteLine("Your answers are " + ans_1 + " " + ans_2 + " " + ans_3);
+            Console.ReadLine();
+
+
+        }
+    }
+}
