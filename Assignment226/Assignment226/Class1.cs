50dd7c8e61aac4d96d78c149187b02daec20855c
Priyanka Thakan <priyankawu@yahoo.com>
2021-03-04 12:00:23 -0800

    Assignment226

---
+++ Assignment226/Assignment226/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment226
+{
+    class Class1
+    {
+
+        public int doMath(int n)
+        {
+            int ans = n * 99;
+            return ans;
+        }
+        //method overloading: same name method but differences
+        public int doMath(decimal n)
+        {
+            decimal ans = n * 99;
+            return (int)ans;
+        }
+
+        public int doMath(string s)
+        {
+            int n = Int32.Parse(s);
+            int ans = n % 6;
+            return ans;
+        }
+    }
+}
---
+++ Assignment226/Assignment226/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment226
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 obj = new Class1();
+            int ans = obj.doMath(10);
+            Console.WriteLine("Answer is " + ans);
+            
+            Class1 obj1 = new Class1();
+            decimal arg = 9.2m;         //m is to denote decimal number: used for banking purposes
+            int ans1 = obj1.doMath(arg);
+            Console.WriteLine("Answer of decimal overloaded method is "+ans1);
+
+            Class1 obj2 = new Class1();
+            int ans2 = obj2.doMath("391");
+            Console.WriteLine("Answer of string overloaded method is " + ans2);
+
+            Console.ReadLine();
+        }
+    }
+}
