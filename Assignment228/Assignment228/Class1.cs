26c6b981fd561940a79782ec39f02298ee64d44c
Priyanka Thakan <priyankawu@yahoo.com>
2021-03-04 13:47:43 -0800

    Assignment228

---
+++ Assignment228/Assignment228/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment228
+{
+    class Class1
+    {
+
+
+        public void doMath(int a, int b)
+        {
+            a = a * a;
+            Console.WriteLine("The second integer is: " + b);
+        }
+    }
+}
---
+++ Assignment228/Assignment228/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment228
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 obj = new Class1();
+            obj.doMath(56, 43);
+
+            int a = 97;
+            int b = 99;
+            obj.doMath(a, b);
+            Console.Read();
+        }
+    }
+}
