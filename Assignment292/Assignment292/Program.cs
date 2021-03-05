18b8e3a6cbd29df80e4ea5e12e37b549ecb3219b
Priyanka Thakan <priyankawu@yahoo.com>
2021-03-05 10:47:02 -0800

    Assignment292NewLMS

---
+++ Assignment292/Assignment292/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment292
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.FirstName = "Sample";           //accessing superclass properties.
+            emp.LastName = "Student";
+            emp.SayName();                      //accessing superclass method.
+            Console.ReadLine();
+        }
+    }
+}
