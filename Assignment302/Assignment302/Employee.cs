5b2776e0032e0e30c19a2e4b0aa980cf0473c072
Priyanka Thakan <priyankawu@yahoo.com>
2021-03-05 16:33:59 -0800

    Assignment302

---
+++ Assignment302/Assignment302/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment302
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name is " + firstName +" "+ lastName);
+
+        }
+    }
+}
---
+++ Assignment302/Assignment302/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment302
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee emp = new Employee();
+            emp.firstName = "Sample";
+            emp.lastName = "Student";
+            emp.SayName();
+            Console.ReadLine();
+        }
+    }
+}
