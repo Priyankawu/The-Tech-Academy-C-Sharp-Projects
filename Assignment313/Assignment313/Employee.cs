1bfb8e8b11fd2e3af8c3573d93b50521ece25f10
Priyanka Thakan <priyankawu@yahoo.com>
2021-03-08 14:39:14 -0800

    Assignment313
    Operator Overloading

---
+++ Assignment313/Assignment313/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment313
+{
+    public class Employee
+    {
+        public int Id;
+
+        public static bool operator ==(Employee a, Employee b)
+        {
+            return a.Id == b.Id;
+        }
+        public static bool operator !=(Employee a, Employee b)
+        {
+            return (a.Id != b.Id);
+        }
+    }
+}
---
+++ Assignment313/Assignment313/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment313
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee a = new Employee();
+            a.Id = 100;
+            
+            Employee b = new Employee();
+            b.Id = 200;
+
+            bool ans = a == b;
+            Console.WriteLine("Boolean answer is " + ans);
+            Console.Read();
+          
+        }
+    }
+}
