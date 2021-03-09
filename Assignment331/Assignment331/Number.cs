62a8fb1f35368915937c17a982cd25158fe64e29
Priyanka Thakan <priyankawu@yahoo.com>
2021-03-09 14:06:00 -0800

    Assignment331
    Struct example

---
+++ Assignment331/Assignment331/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment331
+{
+    struct Number
+    {
+        public Decimal Amount;
+    }
+}
---
+++ Assignment331/Assignment331/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment331
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number n;
+            n.Amount = 23.98m;  //Decimal values have m in the end
+            Console.WriteLine("Struct Number n has Amount: " + n.Amount);
+            Console.ReadLine();
+        }
+    }
+}
