dd2df4a3e30c5a6755832ef00c75b1fd82935d2b
Priyanka Thakan <priyankawu@yahoo.com>
2021-03-09 10:02:50 -0800

    Assignment324
    Enum - changing user input 'string' to enum type.

---
+++ Assignment324/Assignment324/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment324
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week");
+                string sday = Console.ReadLine();
+                
+                DaysOfTheWeek myVar = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), sday);
+                Console.WriteLine("The user typed: "+myVar);
+            }
+           
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            
+            Console.ReadLine();
+            
+        }
+    }
+
+    public enum DaysOfTheWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
