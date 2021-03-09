bed3b2782a67adf9b428c8d85d1f3715d24a5429
Priyanka Thakan <priyankawu@yahoo.com>
2021-03-08 20:47:43 -0800

    Assignment317
    Generic Parameters <T>

---
+++ Assignment317/Assignment317/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment317
+{
+    public class Employee<T> { 
+
+        public List<T> things = new List<T>();
+
+        //prints items from teh list 'things'
+        public void printAll()
+        {
+            foreach (T thing in things)
+            {
+                Console.WriteLine(thing);
+
+            }
+        }
+    }
+}
---
+++ Assignment317/Assignment317/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment317
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating a Employee objects that have an instantiated list called things.
+            Employee<string> one = new Employee<string>();
+            one.things.Add("Priyanka");
+            one.things.Add("Sanjeev");
+            one.things.Add("Hirsh");
+            one.things.Add("Aaria");
+            one.things.Add("Eshvin");
+            Employee<int> two = new Employee<int>();
+            two.things.Add(100);
+            two.things.Add(99);
+            two.things.Add(78);
+            two.things.Add(45);
+            two.things.Add(999);
+            two.things.Add(42);
+            //print the 'things' list of the Employee objects
+            one.printAll();
+            two.printAll();
+            Console.Read();
+        }
+    }
+}
