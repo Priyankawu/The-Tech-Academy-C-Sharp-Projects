d0996865a4f04b0974f21d358d323eb35aa102c6
Priyanka Thakan <priyankawu@yahoo.com>
2021-03-10 09:47:35 -0800

    Assignment337
    Lambda expression

---
+++ Assignment337/Assignment337/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment337
+{
+    public class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public int Id;
+        //constructor takes in parameters and sets the properties
+        public Employee(string f, string l, int i)
+        {
+            firstName = f;
+            lastName = l;
+            Id = i;
+        }
+       
+
+        
+    }
+}
---
+++ Assignment337/Assignment337/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment337
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> empList = new List<Employee>();
+            empList.Add(new Employee("Joe", "Sharma", 0));
+            empList.Add(new Employee("Joe","Gupta",1));
+            empList.Add(new Employee("Priyanka", "Agarwal",2));
+            empList.Add(new Employee("Joe", "Arora",3));
+            empList.Add(new Employee("Hirsh", "Royal",4));
+            empList.Add(new Employee("Aaria", "Royal",5));
+            empList.Add(new Employee("Eshvin", "Royal",6));
+            empList.Add(new Employee("Priyanka", "Royal",7 ));
+            empList.Add(new Employee("Sanjeev", "Royal",8 ));
+            empList.Add(new Employee("Ashish", "Gupta",9 ));
+
+            List<Employee> joes = empList.FindAll(x => x.firstName == "Joe");       //FindAll returns a list
+
+            List<Employee> ids = empList.FindAll(x => x.Id > 5);
+
+            //lambda way of condensing code
+            foreach (Employee joe in empList.FindAll(x => x.firstName == "Joe")) { 
+                Console.WriteLine(joe.firstName);
+            }
+
+            foreach(Employee id5 in empList.FindAll(x => x.Id > 5))
+            {
+                Console.WriteLine(id5.Id);
+            }
+
+            // brute way of doing it
+            //foreach(Employee joe in empList)
+            //{
+            //    if (joe.firstName == "Joe")
+            //    {
+            //        joes.Add(joe);
+            //        Console.WriteLine(joe.firstName);
+            //    }
+            //}
+
+
+          
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
