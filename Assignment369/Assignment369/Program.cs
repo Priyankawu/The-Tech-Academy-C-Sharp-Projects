da13070c0551618175c3672ee27ed9e64c84de70
Priyanka Thakan <priyankawu@yahoo.com>
2021-03-13 10:30:46 -0800

    Assignment369

---
+++ Assignment369/Assignment369/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Assignment369
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine("The time now is " + now.ToString());
+            Console.WriteLine("Please enter a number that I will use to add hours to the current time.");
+
+            int addHours = Convert.ToInt32(Console.ReadLine());
+            int hour = now.Hour+addHours;
+            int minutes = now.Minute;
+            int seconds = now.Second;
+
+            Console.WriteLine("The time after adding your hours is:\n Hours: " + hour + "\n Minutes: " + minutes + "\n Seconds: " + seconds);
+            Console.WriteLine(hour + ":" + minutes + ":" + seconds);
+
+            //DateTime modified = new DateTime(2021, 3, 13, hour, minutes, seconds);   //?? how do I print the time from this easily
+            Console.Read();
+            
+
+        }
+    }
+}
