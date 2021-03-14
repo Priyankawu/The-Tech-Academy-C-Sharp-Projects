20e7ebb0c4d5daf2f40128cfcdb5428598849c1a
Priyanka Thakan <priyankawu@yahoo.com>
2021-03-13 17:28:47 -0800

    Assignment383

---
+++ Assignment383/Assignment383/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment383
+{
+    public class Class1
+    {
+        // const keyword so this variable cannot be changed in the rest of the program.
+        public const string constant = "You cannot change this variable 'constant' in the rest of the program. But I think you can always change it right here.";
+        //trying to experiment with changing the const string.
+        Console.WriteLine("constant " +constant);
+        constant += "Told you. It won't work.";
+
+        //the keyword 'var' can be used only in local declaration e.g. here in this local method??
+        public void localMethod()
+        {
+            var emp = new Employee("First", "Last", 3);
+        }
+
+        
+
+
+        
+    }
+}
---
+++ Assignment383/Assignment383/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment383
+{
+    class Employee
+    {
+
+        public Employee(string fname, string lname, int id) : this(fname,lname,id,10000)
+        {
+            
+        }
+        public Employee(string fname, string lname, int id, int salary)
+        {
+            firstName = fname;
+            lastName = lname;
+            Id = id;
+            Salary = salary;
+        }
+        public string firstName;
+        public string lastName;
+        public int Id;
+        public int Salary;
+    }
+}
