11ab3fa08e8ac09646a9bd6f6454278cbfa2941e
Priyanka Thakan <priyankawu@yahoo.com>
2021-03-15 12:54:28 -0700

    Assignment383secondtry

---
+++ Assginment383secondtry/Assginment383secondtry/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assginment383secondtry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          public const string myConstant = "This is a constant string. You cannot change it in the rest of the code, except here.";
+         Console.WriteLine("What are all these errors?");
+
+        }
+    }
+}
